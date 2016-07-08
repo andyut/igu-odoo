e277158d6319fd3a6ea7402693b8f1cf16dcc2b2
andyutomo1 <andyut@outlook.com>
2016-07-08 10:04:13 +0700

    test for odoo

---
+++ sbo/sbo_customer.py
@@ -0,0 +1,18 @@
+import requests
+import csv
+
+
+
+def getCustomer():
+    url = requests.get('http://192.168.1.171/iguwebapps/app/obp01-data.asp')
+
+
+
+    test = url.iter_lines()
+    reader = csv.reader(test, delimiter=';')
+
+    return reader
+
+print getCustomer()
+
+
