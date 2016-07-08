98bf95e72783c31f3c56ebccd14583fe4e9e2d2a
andyutomo1 <andyut@outlook.com>
2016-07-08 10:27:27 +0700

    test for odoo

--- sbo/sbo_common.py
+++ sbo/sbo_common.py
@@ -4,20 +4,16 @@ import csv
 
 
 def getCustomer():
-    url = requests.get('http://192.168.1.171/iguwebapps/app/obp01-data.asp')
-
-
-
+    # AMBIL DATA CUSTOMER
+    url = requests.get('http://192.168.1.171/iguwebapps/app/SR_getCustomerOutlet.asp')
     test = url.iter_lines()
     reader = csv.reader(test, delimiter=';')
-
     return reader
 
 
 def getProduct():
-     url = requests.get('http://192.168.1.171/iguwebapps/app/Produk/tranM30r-data.asp')
-
-
+    # AMBIL DATA PRODUCT
+    url = requests.get('http://192.168.1.171/iguwebapps/app/Produk/tranM30r-data.asp')
     test = url.iter_lines()
     reader = csv.reader(test, delimiter=';')
     return reader
